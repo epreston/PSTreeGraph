2e7abb1e960937dcb46fc1d5b4fab1f86df4c74f
Ed Preston <epreston@prestonsoft.com>
2012-08-09 06:01:54 +1000

    Updated project, documentation, example class names.
    Separated out the two protocols into separate files.  Showed to
    documentation config some love.  Upgraded things to the latest version.

---
+++ PSTreeGraphView/PSTreeGraphDelegate.h
@@ -0,0 +1,14 @@
+//
+//  PSTreeGraphDelegate.h
+//  PSHTreeGraph
+//
+//  Created by Ed Preston on 9/08/12.
+//  Copyright (c) 2012 Preston Software. All rights reserved.
+//
+
+#ifndef PSHTreeGraph_PSTreeGraphDelegate_h
+#define PSHTreeGraph_PSTreeGraphDelegate_h
+
+
+
+#endif
